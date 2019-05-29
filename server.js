b4bfc3c12108f9f70d87a42ed9c1cc9a4299ee3d
Weliton Lima <welitonlima@consulplan.br>
2019-05-29 16:47:02 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+// Iniciando o App
+const app = express();
+app.use(express.json());// permissão para enviar json
+app.use(cors());
+
+// Iniciando o DB
+mongoose.connect('mongodb://localhost/nodeapi', { useNewUrlParser: true});
+
+requireDir('./src/models');
+
+// Rotas
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
